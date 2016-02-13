e2e959b5db67366765f0577d604e0794709f3dfa
Michael Schaarschmidt <michael@schaaryworks.com>
2016-02-13 23:05:29 +0100

    rearrange commands

--- elixir_server.sh
+++ elixir_server.sh
@@ -1,6 +1,7 @@
 /usr/bin/apt-get update
 /usr/bin/apt-get upgrade -y
 /usr/bin/apt-get install -y \
+  build-essential \
   libcurl4-gnutls-dev \
   python-pycurl-dbg \
   python-pycurl \
@@ -26,7 +27,6 @@ if [ $? -ne 0 ]; then echo "Installation von python-software-properties fehlgesc
 if [ $? -ne 0 ]; then echo "Installation von python-software-properties fehlgeschlagen!"; fi
 /usr/bin/apt-get update
 /usr/bin/apt-get install -y \
-  build-essential \
   zlib1g \
   zlib1g-dev \
   libreadline6 \
@@ -41,33 +41,33 @@ if [ $? -ne 0 ]; then echo "Installation von python-software-properties fehlgesc
   inotify-tools \
   git-core \
   inotify-tools
-#
-#/usr/bin/curl -O \
-#  https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && \
-#  dpkg -i erlang-solutions_1.0_all.deb
-#
-#/usr/bin/apt-get update
-#/usr/bin/apt-get install -y \
-#  erlang-dev \
-#  erlang-ssl \
-#  erlang-inets \
-#  erlang-dev \
-#  erlang-edoc \
-#  erlang-eunit \
-#  erlang-tools \
-#  erlang-common-test \
-#  esl-erlang \
-#  erlang-parsetools-dev \
-#  elixir
-#
-#/usr/bin/bash -c '/usr/bin/echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-#
-#/usr/bin/curl -s -o- https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
-#  /usr/bin/apt-key add -
-#
-#/usr/bin/apt-get update
-#/usr/bin/apt-get upgrade -y
-#/usr/bin/apt-get install -y postgresql-9.4
-#
-#/usr/bin/curl -sL https://deb.nodesource.com/setup_5.x | bash -
-#/usr/bin/apt-get install -y nodejs
+
+/usr/bin/curl -O \
+  https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && \
+  dpkg -i erlang-solutions_1.0_all.deb
+
+/usr/bin/apt-get update
+/usr/bin/apt-get install -y \
+  erlang-dev \
+  erlang-ssl \
+  erlang-inets \
+  erlang-dev \
+  erlang-edoc \
+  erlang-eunit \
+  erlang-tools \
+  erlang-common-test \
+  esl-erlang \
+  erlang-parsetools-dev \
+  elixir
+
+/usr/bin/bash -c '/usr/bin/echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+/usr/bin/curl -s -o- https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
+  /usr/bin/apt-key add -
+
+/usr/bin/apt-get update
+/usr/bin/apt-get upgrade -y
+/usr/bin/apt-get install -y postgresql-9.4
+
+/usr/bin/curl -sL https://deb.nodesource.com/setup_5.x | bash -
+/usr/bin/apt-get install -y nodejs
