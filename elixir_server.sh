fe028938858e7a48746db0bcc1cb1c8380fe8463
Michael Schaarschmidt <michael@schaaryworks.com>
2016-02-13 23:39:39 +0100

    first release candidate

--- elixir_server.sh
+++ elixir_server.sh
@@ -10,40 +10,31 @@
   software-properties-common \
   python-software-properties
 
-#if [ $? -ne 0 ]; then echo "Installation von python-software-properties fehlgeschlagen!"; fi
-#/usr/bin/add-apt-repository -y ppa:ubuntu-elisp/ppa
-#if [ $? -ne 0 ]; then echo "Installation von python-software-properties fehlgeschlagen!"; fi
-#/usr/bin/add-apt-repository -y ppa:fish-shell/release-2
-#if [ $? -ne 0 ]; then echo "Installation von python-software-properties fehlgeschlagen!"; fi
-#/usr/bin/add-apt-repository -y ppa:git-core/ppa
-#if [ $? -ne 0 ]; then echo "Installation von python-software-properties fehlgeschlagen!"; fi
-#/usr/bin/add-apt-repository -y ppa:nginx/stable
-#if [ $? -ne 0 ]; then echo "Installation von python-software-properties fehlgeschlagen!"; fi
-#/usr/bin/add-apt-repository -y ppa:mizuno-as/silversearcher-ag
-#if [ $? -ne 0 ]; then echo "Installation von python-software-properties fehlgeschlagen!"; fi
-#/usr/bin/add-apt-repository -y ppa:neovim-ppa/unstable
-#if [ $? -ne 0 ]; then echo "Installation von python-software-properties fehlgeschlagen!"; fi
-#/usr/bin/add-apt-repository -y ppa:chris-lea/redis-server
-#if [ $? -ne 0 ]; then echo "Installation von python-software-properties fehlgeschlagen!"; fi
-#/usr/bin/apt-add-repository -y ppa:chris-lea/node.js
-#if [ $? -ne 0 ]; then echo "Installation von python-software-properties fehlgeschlagen!"; fi
-#/usr/bin/apt-get update
-#/usr/bin/apt-get install -y \
-#  zlib1g \
-#  zlib1g-dev \
-#  libreadline6 \
-#  libreadline6-dev \
-#  silversearcher-ag \
-#  emacs-snapshot \
-#  fish \
-#  htop \
-#  nginx \
-#  neovim \
-#  ca-certificates \
-#  inotify-tools \
-#  git-core \
-#  inotify-tools
-#
+/usr/bin/add-apt-repository -y ppa:ubuntu-elisp/ppa
+/usr/bin/add-apt-repository -y ppa:fish-shell/release-2
+/usr/bin/add-apt-repository -y ppa:git-core/ppa
+/usr/bin/add-apt-repository -y ppa:nginx/stable
+/usr/bin/add-apt-repository -y ppa:mizuno-as/silversearcher-ag
+/usr/bin/add-apt-repository -y ppa:neovim-ppa/unstable
+/usr/bin/add-apt-repository -y ppa:chris-lea/redis-server
+/usr/bin/apt-add-repository -y ppa:chris-lea/node.js
+/usr/bin/apt-get update
+/usr/bin/apt-get install -y \
+  zlib1g \
+  zlib1g-dev \
+  libreadline6 \
+  libreadline6-dev \
+  silversearcher-ag \
+  emacs-snapshot \
+  fish \
+  htop \
+  nginx \
+  neovim \
+  ca-certificates \
+  inotify-tools \
+  git-core \
+  inotify-tools
+
 /usr/bin/curl -O \
   https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && \
   dpkg -i erlang-solutions_1.0_all.deb
